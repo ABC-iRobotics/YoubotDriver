bb14e23a0c6089f18cabbe9e835f024db6cb937a
JKuti <kutij6@gmail.com>
2022-11-11 14:38:21 +0100

    Bugfix

--- fromscratch/YoubotConfig.hpp
+++ fromscratch/YoubotConfig.hpp
@@ -8,17 +8,17 @@ typedef std::map<std::string, double> NameValueMap;
 
 struct YoubotConfig {
   int gripperIndex, jointIndices[5];
-  
-  int& shoulderIndex = jointIndices[0];
-  int& waistIndex = jointIndices[1];
+
+  int& waistIndex = jointIndices[0];
+  int& shoulderIndex = jointIndices[1];
   int& elbowIndex = jointIndices[2];
   int& wristPitchIndex = jointIndices[3];
   int& wristYawIndex = jointIndices[4];
 
   NameValueMap jointConfigs[5];
-  
-  NameValueMap& shoulderConfig = jointConfigs[0];
-  NameValueMap& waistConfig = jointConfigs[1];
+
+  NameValueMap& waistConfig = jointConfigs[0];
+  NameValueMap& shoulderConfig = jointConfigs[1];
   NameValueMap& elbowConfig = jointConfigs[2];
   NameValueMap& wristPitchConfig = jointConfigs[3];
   NameValueMap& wristYawConfig = jointConfigs[4];
