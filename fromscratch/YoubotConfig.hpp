0e68e2a3ce0975b8dc3f7b42aedac176c26f2af8
JKuti <kutij6@gmail.com>
2022-11-11 12:20:33 +0100

    To allow iterations in the config

--- fromscratch/YoubotConfig.hpp
+++ fromscratch/YoubotConfig.hpp
@@ -7,9 +7,21 @@
 typedef std::map<std::string, double> NameValueMap;
 
 struct YoubotConfig {
-  int gripperIndex, shoulderIndex, waistIndex, elbowIndex, wristPitchIndex, wristYawIndex;
+  int gripperIndex, jointIndices[5];
+  
+  int& shoulderIndex = jointIndices[0];
+  int& waistIndex = jointIndices[1];
+  int& elbowIndex = jointIndices[2];
+  int& wristPitchIndex = jointIndices[3];
+  int& wristYawIndex = jointIndices[4];
 
-  NameValueMap shoulderConfig, waistConfig, elbowConfig, wristPitchConfig, wristYawConfig;
+  NameValueMap jointConfigs[5];
+  
+  NameValueMap& shoulderConfig = jointConfigs[0];
+  NameValueMap& waistConfig = jointConfigs[1];
+  NameValueMap& elbowConfig = jointConfigs[2];
+  NameValueMap& wristPitchConfig = jointConfigs[3];
+  NameValueMap& wristYawConfig = jointConfigs[4];
 
   YoubotConfig(const std::string& filename);
 };
