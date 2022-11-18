1e238d4d3e9585b3c76e2ad71ea0f2fd91190184
József Kuti <kutij6@gmail.com>
2022-11-18 16:02:36 +0100

    Example project: folder structure is not the best, but does work...

---
+++ example/tester.cpp
@@ -0,0 +1,47 @@
+#include "adapters.hpp"
+#include "YoubotManipulator.hpp"
+#include "Time.hpp"
+#include "Logger.hpp"
+#include "Eigen/dense" // just to test it...
+
+int main(int argc, char *argv[])
+{
+  // Get Configfile
+  YoubotConfig config("C:/Users/kutij/Desktop/myYouBotDriver/src/lowlevelcontrol/youBotArmConfig_fromKeisler.json");
+  //youBotArmConfig_fromfactory.json");
+  //youBotArmConfig_fromMoveIt.json");
+  //youBotArmConfig_fromKeisler.json");
+
+  // Initialize logger
+  Log::Setup(config.logConfig);
+
+  // Find appropriate ethernet adapter and open connection
+  {
+    char name[1000];
+    if (findYouBotEtherCatAdapter(name))
+      log(Log::info, "Adapter found:" + std::string(name));
+    else {
+      log(Log::fatal, "Adapter with turned on youBot arm NOT found!");
+      return -1;
+    }
+    if (!EtherCATMaster::GetSingleton()->OpenConnection(name))
+      return -1;
+  }
+
+  YoubotManipulator man(config, EtherCATMaster::GetSingleton());
+  man.ConfigJoints();
+  // if (man.CheckJointConfigs())  std::cout << "OK!!" << std::endl;
+  man.InitializeAllJoints();
+  man.Calibrate();
+  
+  SLEEP_SEC(1);
+  man.ResetErrorFlags();
+  EtherCATMaster::GetSingleton()->StartProcessThread(30);
+
+  man.ReqJointPosition(-10, 10, 10, 10, 0);
+  SLEEP_SEC(5);
+
+  EtherCATMaster::GetSingleton()->StopProcessThread();
+
+  return 0;
+}
