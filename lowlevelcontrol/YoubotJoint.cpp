97acfeef782f626f1a12b8488b3bd57043387302
JKuti <kutij6@gmail.com>
2023-01-08 16:47:00 +0100

    Now controller firmware/type check seems robust

--- lowlevelcontrol/YoubotJoint.cpp
+++ lowlevelcontrol/YoubotJoint.cpp
@@ -10,8 +10,15 @@ using namespace youbot::intrinsic;
 
 void YoubotJoint::_getFirmwareVersionViaMailbox() {
   auto ptr = GetFirmware::InitSharedPtr(slaveIndex);
-  center->SendMessage_(ptr);
-  ptr->GetOutput(controllerNum, firmwareversion);
+  int n_try = 0;
+  do {
+    SLEEP_MILLISEC(5)
+    center->SendMessage_(ptr);
+    ptr->GetOutput(controllerNum, firmwareversion);
+    n_try++;
+    if (n_try>=50)
+      throw std::runtime_error("Controller/firmware type zeros returned again and again");
+  } while (controllerNum == 0 && firmwareversion == 0);
   if (controllerNum != 1610 || firmwareversion != 148) {
     log(Log::fatal, "Not supported joint with slaveindex " + std::to_string(slaveIndex) + ". Controller: "
       + std::to_string(controllerNum) + " Firmware: " + std::to_string(firmwareversion));
