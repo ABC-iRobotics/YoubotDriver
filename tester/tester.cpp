d8f9eb2ef375797852461c0b6a41d579b7b89bf7
JKuti <kutij6@gmail.com>
2023-01-22 20:19:00 +0100

    It seems that POSITION_MODE (w firmware 1.48) causes the radom encoder errors

--- tester/tester.cpp
+++ tester/tester.cpp
@@ -30,6 +30,22 @@ void StopThere(YoubotManipulator& man, int time_ms) {
     std::chrono::steady_clock::now() - start).count() < time_ms);
 }
 
+void GoTowardsZero(YoubotManipulator& man, int time_ms, double velocityRadPerSec, YoubotConfig& config) {
+  log(Log::info, "GoTowardsZero!");
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < 5; i++) {
+    bool backward = bool(config.jointConfigs[i].at("CalibrationDirection"))
+      ^ bool(config.jointConfigs[i].at("qDirectionSameAsEnc"));
+    man.GetJoint(i)->ReqJointSpeedRadPerSec(backward ? velocityRadPerSec : -velocityRadPerSec);
+  }
+  do {
+    EtherCATMaster::GetSingleton()->ExchangeProcessMsg();
+    man.LogStatusProcess();
+    SLEEP_MILLISEC(5)
+  } while (std::chrono::duration_cast<std::chrono::milliseconds>(
+    std::chrono::steady_clock::now() - start).count() < time_ms);
+}
+
 void FreeDriveMode(YoubotManipulator& man, int time_ms) {
   log(Log::info, "FreeDriveMode!");
   auto start = std::chrono::steady_clock::now();
@@ -54,6 +70,18 @@ void WaveDemo(YoubotManipulator& man, int time_ms) {
       14. / 180. * M_PI * sin(2. * M_PI / 4. * t),
       16. / 180. * M_PI * sin(2. * M_PI / 4.5 * t),
       18. / 180. * M_PI * sin(2. * M_PI / 5. * t));
+    EtherCATMaster::GetSingleton()->ExchangeProcessMsg();
+    man.LogStatusProcess();
+    SLEEP_MILLISEC(5)
+  } while (std::chrono::duration_cast<std::chrono::milliseconds>(
+    std::chrono::steady_clock::now() - start).count() < time_ms);
+}
+
+void ZeroVelocityDemo(YoubotManipulator& man, int time_ms) {
+  log(Log::info, "ZeroVelocityDemo!");
+  auto start = std::chrono::steady_clock::now();
+  man.ReqJointSpeedRadPerSec(0, 0, 0, 0, 0);
+  do {
     EtherCATMaster::GetSingleton()->ExchangeProcessMsg();
     man.LogStatusProcess();
     SLEEP_MILLISEC(5)
@@ -94,17 +122,21 @@ int main(int argc, char *argv[])
   SLEEP_SEC(1);
   man.ResetErrorFlags();
   
-  // Go to zero and stay there
-  GoToZero(man, 3000);
-  /*
-  // Stop at the given position
-  StopThere(man, 3000);
+  GoTowardsZero(man, 5000, 0.05, config);
 
+  //ZeroVelocityDemo(man, 5000);
+
+  // Go to zero and stay there
+  //GoToZero(man, 3000);
+  
+  // Stop at the given position
+  //StopThere(man, 10000);
+  /*
   // Zero current mode: ~free drive
   FreeDriveMode(man, 3000);*/
 
   // Wave movement
-  WaveDemo(man, 10000);
+  //WaveDemo(man, 10000);
 
   return 0;
 }
