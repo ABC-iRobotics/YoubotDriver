c22c36c88f1e431f84751b048c645e4704fd2f06
József Kuti <kutij6@gmail.com>
2023-01-26 13:58:10 +0100

    to avoid warnings

--- lowlevelcontrol/adapters.cpp
+++ lowlevelcontrol/adapters.cpp
@@ -191,7 +191,7 @@ char* SDO2string(uint16 slave, uint16 index, uint8 subidx, uint16 dtype)
       break;
     case ECT_INTEGER64:
       i64 = (int64*)&usdo[0];
-      sprintf(str, "0x%16.16 / %u", *i64, *i64);
+      sprintf(str, "0x%16.16 / %I64u", *i64, *i64);
       break;
     case ECT_UNSIGNED8:
       u8 = (uint8*)&usdo[0];
