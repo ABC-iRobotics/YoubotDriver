2f3165931e9632ff5122b381a97b4fc9eb331821
József Kuti <kutij6@gmail.com>
2023-01-30 13:43:28 +0100

    ManipulatorTask.hpp some comments

--- manipulatormodul/ManipulatorTask.hpp
+++ manipulatormodul/ManipulatorTask.hpp
@@ -8,6 +8,8 @@
 
 namespace youbot {
 
+  // The ManipulatorCommand is a low level settings for the manipulator, e.g. use given joint velocity
+  // it is defined by a vector (e.g. joint velocities) and the type specifier e.g. "joint velocity"
   struct ManipulatorCommand {
     enum Type {
       JOINT_POSITION, //forbidden
@@ -20,20 +22,23 @@ namespace youbot {
     ManipulatorCommand(Type type, const Eigen::VectorXd& value);
   };
 
+  // The manipulator task is a user defined (overridden) object that provides ManipulatorCommands
+  // for the current time moment and current manipulator status
   class ManipulatorTask {
   public:
     enum TaskType {
       INITIALIZATION,
       STOPPED,
-      RAW_CONSTANT_JOINTSPEED
+      RAW_CONSTANT_JOINTSPEED // currently these types are defined
     };
     static std::string Type2String(TaskType type);
-    
-    void Initialize(const JointsState& start_state_); // Called by the motion layer
-
-    virtual ManipulatorCommand GetCommand(const JointsState& new_state) = 0; // performs the task after it zero velocity commands
 
     virtual TaskType GetType() const = 0;
+
+    void Initialize(const JointsState& start_state_); // Called by the motion layer to setup start moment, and start state
+
+    virtual ManipulatorCommand GetCommand(const JointsState& new_state) = 0;
+    // called again and again by the thread to give commands to the manipulator. Must contain the control logic
 
     bool Finished() const;
 
@@ -58,20 +63,3 @@ namespace youbot {
   };
 }
 #endif
-
-
-/*
-
-
-    // Longer operations
-    void MoveToPosition_RawPID(const Eigen::VectorXd& target, double timelimit) { // todo a stop mechanism??
-
-    }
-
-    void MoveToPosition_PIDwSpeedRampant(const double target[5], const double maxspeed[5]) {
-
-    }
-
-    void MoveToPosition_JointInterpolated(const double target[5]) {
-
-    }*/
