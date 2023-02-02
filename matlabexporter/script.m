253ae8d16ad54d48d15f50980e7523b9fee77daf
JKuti <kutij6@gmail.com>
2023-02-02 14:25:28 +0100

    Matlab destructor problems solved for virtual robot

--- matlabexporter/script.m
+++ matlabexporter/script.m
@@ -2,6 +2,12 @@ virtual = 1;
 obj = YoubotManager("../config/youBotArmConfig_fromKeisler.json",virtual);
 obj.StartThread();
 obj.StartPlot;
+
+%%
+clear all
+%%
+obj.StartThread();
+obj.StartPlot;
 %%
 obj.FreeDrive(100);
 %%
@@ -12,12 +18,7 @@ obj.SetJointVelocity([1 1 -1 1 -1]*(5),10);
 obj.StopJoints();
 %%
 [q,dq,tau,mode] = GetStatus(obj);
-%% 
-q'
-dq'
-tau'
-mode
-
+q',dq',tau',mode
 %%
 obj.SetJointVelocity(-[1 1 -1 -1 1]*0.2,10);
 obj.StopJoints();
