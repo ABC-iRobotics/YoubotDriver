9ad4f111a49b7167316f527e6a953036d992fa1d
József Kuti <kutij6@gmail.com>
2023-02-06 10:03:13 +0100

    linewidth..

--- matlabexporter/MovingLine.m
+++ matlabexporter/MovingLine.m
@@ -13,7 +13,7 @@ classdef MovingLine < handle
         
         function Move(obj,l,str)
             if (~obj.initialized)
-                obj.object = plot3(l(1,:),l(2,:),l(3,:),str);
+                obj.object = plot3(l(1,:),l(2,:),l(3,:),str,'linewidth',3);
                 obj.initialized = 1;
             else
                 obj.object.XData = l(1,:);
