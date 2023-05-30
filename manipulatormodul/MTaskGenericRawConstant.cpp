7ad64c149e03a8072d175d82797dd3b49655be50
József Kuti <kutij6@gmail.com>
2023-05-30 17:37:01 +0200

    MTaskGenericRawConstant introduced again

--- manipulatormodul/MTaskGenericRawConstant.cpp
+++ manipulatormodul/MTaskGenericRawConstant.cpp
@@ -4,45 +4,64 @@
 
 using namespace youbot;
 
-youbot::MTaskGenericRawConstant::MTaskGenericRawConstant(const std::vector<Cmd>& cmd, double P_constant) : cmd(cmd), P_constant(P_constant) {}
+youbot::MTaskGenericRawConstant::MTaskGenericRawConstant(double P_constant) : P_constant(P_constant) {}
+
+void youbot::MTaskGenericRawConstant::SetCommand(const std::vector<Cmd>& cmd, double time_limit_s) {
+  std::lock_guard<std::mutex> lock(target_mutex);
+  for (int i = 0; i < 5; i++)
+    target_cmd[i] = cmd[i];
+  target_time = std::chrono::system_clock::now() + std::chrono::microseconds(long long(1000000 * time_limit_s));
+}
 
 ManipulatorCommand youbot::MTaskGenericRawConstant::GetCommand(
   const JointsState& new_state) {
+  // Load defended targets, check timeout
+  Cmd cmd_now[5];
+  {
+    std::lock_guard<std::mutex> lock(target_mutex);
+    if (target_time < std::chrono::system_clock::now())
+      for (int i = 0; i < 5; i++)
+        target_cmd[i] = {};
+    for (int i = 0; i < 5; i++)
+      cmd_now[i] = target_cmd[i];
+  }
+
   BLDCCommand cmd_out[5];
   double pos;
   for (int i = 0; i < 5; i++) {
     bool pos_mode = true;
-    switch (cmd[i].type)
+    switch (cmd_now[i].type)
     {
     case STOP:
       cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_STOP, 0);
       break;
+      /*
     case POSITION_MOTOR_TICK:
       cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_RPM,
-        P_constant * (cmd[i].value - new_state.joint[i].motorticks.value) / 4000. * 60.);
+        P_constant * (cmd_now[i].value - new_state.joint[i].motorticks.value) / 4000. * 60.);
       break;
     case POSITION_MOTOR_RAD:
       cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_RPM,
-        P_constant * (cmd[i].value / M_PI / 2. - new_state.joint[i].motorticks.value / 4000.) * 60.);
+        P_constant * (cmd_now[i].value / M_PI / 2. - new_state.joint[i].motorticks.value / 4000.) * 60.);
       break;
     case VELOCITY_MOTOR_RPM:
-      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_RPM, cmd[i].value);
+      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_RPM, cmd_now[i].value);
       break;
     case TORQUE_MOTOR_MA:
-      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_CURRENT_MA, cmd[i].value);
+      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_CURRENT_MA, cmd_now[i].value);
       break;
     case TORQUE_MOTOR_NM:
-      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_TORQUE_NM, cmd[i].value); // TODO... 
-      break;
+      cmd_out[i] = BLDCCommand(BLDCCommand::MOTOR_TORQUE_NM, cmd_now[i].value); // TODO... 
+      break;*/
     case POSITION_JOINT_RAD:
       cmd_out[i] = BLDCCommand(BLDCCommand::JOINT_VELOCITY,
-        P_constant * (cmd[i].value - new_state.joint[i].q.value));
+        P_constant * (cmd_now[i].value - new_state.joint[i].q.value));
       break;
     case VELOCITY_JOINT_RADPERSEC:
-      cmd_out[i] = BLDCCommand(BLDCCommand::JOINT_VELOCITY, cmd[i].value);
+      cmd_out[i] = BLDCCommand(BLDCCommand::JOINT_VELOCITY, cmd_now[i].value);
       break;
     case TORQUE_JOINT_NM:
-      cmd_out[i] = BLDCCommand(BLDCCommand::JOINT_TORQUE, cmd[i].value);
+      cmd_out[i] = BLDCCommand(BLDCCommand::JOINT_TORQUE, cmd_now[i].value);
       break;
     default:
       throw std::runtime_error("not defined type");
--- manipulatormodul/MTaskGenericRawConstant.hpp
+++ manipulatormodul/MTaskGenericRawConstant.hpp
@@ -3,6 +3,7 @@
 
 #include "MTask.hpp"
 #include <vector>
+#include <mutex>
 
 namespace youbot {
   /// <summary>
@@ -25,13 +26,19 @@ namespace youbot {
     struct Cmd {
       CmdType type;
       double value;
+      Cmd(CmdType type_, double value_) :type(type_), value(value_) {}
+      Cmd() : type(CmdType::STOP), value(0) {};
     };
   private:
-    const std::vector<Cmd> cmd;
+    Cmd target_cmd[5] = { {}, {}, {}, {}, {} };
+    std::chrono::system_clock::time_point target_time = std::chrono::system_clock::now();
+    std::mutex target_mutex;
     const double P_constant;
+
   public:
-    MTaskGenericRawConstant(const std::vector<Cmd>& cmd,
-      double P_constant = 500. / 4000. * 60. * 0.1);
+    MTaskGenericRawConstant(double P_constant = 500. / 4000. * 60. * 0.1);
+
+    void SetCommand(const std::vector<Cmd>& cmd, double time_limit_s);
 
     ManipulatorCommand GetCommand(const JointsState& new_state) override;
 
